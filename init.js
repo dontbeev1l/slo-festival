eff3cb4a854277f32f3c528d2dad672a1108e23f
dontbeev1l <42718214+dontbeev1l@users.noreply.github.com>
2022-07-18 11:27:46 +0300

    Ininit clean

--- init.js
+++ init.js
@@ -1,14 +1,4 @@
-/*
-<div class="more__range range">
-    <input type="number" value="1" class="range__input">
-    <div class="range__control control_minus">-</div>
-    <div class="range__control control_plus">+</div>
-    <span class="range__value">1</span>
-</div>
-*/
-
-const modulesLoader = new ModulesLoader();
-modulesLoader
+new ModulesLoader()
     .loadModule('random')
     .loadModule('dom')
     .loadModule('background')
--- views.js
+++ views.js
@@ -1,52 +1,13 @@
-const backgrounds = new BackgroundsModule(['main', 'menu']);
+const backgrounds = new BackgroundsModule(['main', 'game']);
 
 
 const S = Storage.create(
     {
         balance: 500,
         bet: 10,
-        music: true,
-        buys: {
-            blue: true
-        },
-        scoop: 'b',
-        collection: {
-
-        }
-    },
-    {
-        bet: (value, _S) => {
-            if (value < 10) {
-                _S.bet = 10;
-            }
-
-            if (value > _S.balance) {
-                _S.bet = Math.floor(_S.balance / 10) * 10;
-            }
-        },
-
-        balance: (value, _S) => {
-            if (value < 10) {
-                _S.balance = 100;
-            }
-        },
-
-        collection: (value) => {
-            Object.keys(value).forEach(key => {
-                if (!value[key]) { return; }
-                q('.collection img')[+key - 1].activate();
-            })
-        }
+        music: true
     }
 );
-
-const MUSICS = ['./music/1.mp3', './music/2.mp3']
-let musicIndex = 0;
-
-function play() { audioEl.play(); pauseEl.style.display = 'none'; S.music = true; }
-function pause() { audioEl.pause(); pauseEl.style.display = 'block'; S.music = false; }
-function pauseView() { audioEl.pause(); pauseEl.style.display = 'play'; }
-function playNext() { musicIndex = (musicIndex + 1) % MUSICS.length; audioEl.src = MUSICS[musicIndex]; play(); }
 
 new View('license',
     () => {
@@ -56,29 +17,14 @@ new View('license',
 
 new View('menu',
     () => {
-        backgrounds.setActive('menu');
+        backgrounds.setActive('main');
     }
 );
 
 new View('game',
     () => {
-        backgrounds.setActive('menu');
-        if (S.music) {
-            play()
-        } else {
-            pause();
-        }
+        backgrounds.setActive('main');
     },
     () => {
-        pauseView();
     }
 );
-
-
-new View('shop', () => {
-    backgrounds.setActive('menu');
-});
-
-new View('collection', () => {
-    backgrounds.setActive('menu');
-});
